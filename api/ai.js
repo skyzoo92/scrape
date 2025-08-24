61f5115ffd8dc53399b2993fbd9e7ba5a5f005cf
Hydra <145371709+skyzoo92@users.noreply.github.com>
2025-08-24 16:04:57 +0700

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -1,31 +1,31 @@
 const fetch = require('node-fetch');
 
  async function askAI(question, model = 'llama-3.3', systemPrompt = null) {
- try {
-  const res = await fetch("https://ai-interface.anisaofc.my.id/api/chat", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      question: question,
-      model: model,
-      system_prompt: promt
-    })
-  });
+  try {
+    const res = await fetch("https://ai-interface.anisaofc.my.id/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        question,
+        model,
+        system_prompt: systemPrompt
+      })
+    });
 
-  const data = await res.json();
-  return {
-     status: true,
-     creator: "@kelvdra/scraper",
-     results: data.response
-     }
+    const data = await res.json();
+    return {
+      status: true,
+      creator: "@kelvdra/scraper",
+      results: data?.response || null
+    };
   } catch (err) {
     return {
-    status: false,
-    creator: "@kelvdra/scraper",
-    result: err
-    }
+      status: false,
+      creator: "@kelvdra/scraper",
+      results: err.message
+    };
   }
 }
 
